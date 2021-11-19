cb838d09fa811bd6fda9e960b5d1249b8fe569d6
Marcelo <Marcelo@DESKTOP-P6H1NN8>
2021-11-19 00:54:07 -0300

    Criação do Model e implementado o log4j2

---
+++ LojaVirtual/src/main/java/br/com/mvtsolucoes/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,17 @@
+package br.com.mvtsolucoes.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "marca_produto")
+@SequenceGenerator( name= "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue= 1 )
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
