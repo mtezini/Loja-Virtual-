63c8cc3f495da55929456af575148a7a8d493637
Marcelo <Marcelo@DESKTOP-P6H1NN8>
2021-12-07 20:01:18 -0300

    Editado

--- LojaVirtual/src/main/java/br/com/mvtsolucoes/lojavirtual/model/Pessoa.java
+++ LojaVirtual/src/main/java/br/com/mvtsolucoes/lojavirtual/model/Pessoa.java
@@ -7,18 +7,17 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
+
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
